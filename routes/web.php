fd54c850c205d70cf912ad6f5e669923ccb58d15
Frank-CKA <fchongkan@gmail.com>
2023-04-13 23:08:58 -0600

    ruta get a post

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,7 @@ Route::get('/actividadesNimbu', [App\Http\Controllers\ClienteController::class, 
 Route::get('/galeriaNimbu', [App\Http\Controllers\ClienteController::class, 'galeria'])->name('NimbuGaleria');
 Route::get('/galeriaActividadNimbu', [App\Http\Controllers\ClienteController::class, 'galeriaActividad'])->name('NimbuGaleriaActividad');
 Route::get('/reservacionNimbu/{idCapacitacion?}', [App\Http\Controllers\ClienteController::class, 'reservarCapacitacion'])->name('reservarCapacitacion');
-Route::get('/reservaCliente', [App\Http\Controllers\ClienteController::class, 'reservaCliente'])->name('reservaCliente'); //Cambio de método POST a GET
-Route::post('/reservaCliente', [App\Http\Controllers\ClienteController::class, 'reservaCliente'])->name('reservaCliente'); //ruta extra con método POST
+Route::post('/reservaCliente', [App\Http\Controllers\ClienteController::class, 'reservaCliente'])->name('reservaCliente'); //Cambio de método de GET a POST
 Route::get('/galeriaNimbuFiltro/{filtro?}', [App\Http\Controllers\ClienteController::class, 'galeriaFiltro'])->name('NimbuGaleriaFiltro#2');
 Route::get('/galeriaActividadNimbuFiltro/{filtro?}', [App\Http\Controllers\ClienteController::class, 'galeriaActividadFiltro'])->name('NimbuGaleriaActividadFiltro');
 
